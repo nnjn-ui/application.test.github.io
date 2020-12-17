dacbc013ef9219a69445a974a33875bdddbb7de7
nnjn <danil.bygay@gmail.com>
2020-12-17 17:53:23 +0100

    QuizList

--- react-quiz/src/containers/QuizList/QuizList.js
+++ react-quiz/src/containers/QuizList/QuizList.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react'
+import classes from './QuizList.css'
 
 export default class QuizList extends Component {
+
+    renderQuizes() {
+        return [1, 2, 3].map(() => {
+            return (
+                <li key={index}>
+                    <NavLink> 
+                        Тест {quiz}
+                    </NavLink>
+                </li>
+            )
+        })
+    }
+
     render() {
         return (
-            <div>
-                <h1>Quiz List</h1>
+            <div className={classes.QuizList}>
+                <div>
+                    <h1>Список тестов</h1>
+
+                    <ul>
+                        { this.renderQuizes() }
+                    </ul>
+                </div>
             </div>
         )
     }
