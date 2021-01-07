f051067351952c113b58f8e50b4ec20c65960579
nnjn <danil.bygay@gmail.com>
2021-01-07 18:01:02 +0100

    upload file

--- react-quiz/src/index.js
+++ react-quiz/src/index.js
@@ -4,13 +4,19 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'react-redux'
+import {Provider} from 'react-redux'
+import rootReducer from './store/reducers/rootReducer'
 
+const store = createStore(rootReducer)
 
 
 const app = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>   
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>  
+    </Provider> 
 )
 
 ReactDOM.render(app, document.getElementById('root'));
