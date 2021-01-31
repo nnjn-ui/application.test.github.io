60d80f624160d6762f383d662060e4bc461688d8
nnjn <danil.bygay@gmail.com>
2021-01-31 15:53:03 +0100

    update Redux

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,26 +1,62 @@
 import React, { Component } from 'react';
 import Layuot from './hoc/Layout/Layout'
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch, Redirect, withRouter} from 'react-router-dom'
 import Quiz from './containers/Quiz/Quiz'
 import QuizList from './containers/QuizList/QuizList'
 import Auth from './containers/Auth/Auth'
 import QuizCreator from './containers/QuizCreator/QuizCreator'
+import {connect} from 'react-redux'
+import Logout from './components/Logout/Logout'
+
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.autoLogin()
+  }
+
   render() {
+
+    let routes = (
+      <Switch>
+      <Route path="/auth" component={Auth} />
+      <Route path="/quiz/:id" component={Quiz} />
+      <Route path="/" component={QuizList} />
+      <Redirect to="/" />
+    </Switch>
+    )
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+        
+        <Route path="/quiz-creator" component={QuizCreator} />
+        <Route path="/quiz/:id" component={Quiz} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/" component={QuizList} />
+        <Redirect to="/" />
+      </Switch>
+      )
+    }
+
     return (
       <Layuot>
-        <Switch>
-          <Route path="/auth" component={Auth} />
-          <Route path="/quiz-creator" component={QuizCreator} />
-          <Route path="/quiz/:id" component={Quiz} />
-          <Route path="/" component={QuizList} />
-
-        </Switch>
-
+       { routes }
       </Layuot>
     );
   }
 }
 
-export default App
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: !!state.auth.token
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    autoLogin: () => dispatch(autoLogin())
+  }
+}
+
+export default withRouter(connect(mapStateToProps)(App))
--- react-quiz/src/components/Navigation/Drawer/Drawer.js
+++ react-quiz/src/components/Navigation/Drawer/Drawer.js
@@ -4,11 +4,7 @@ import classes from './Drawer.css'
 import {NavLink} from 'react-router-dom'
 import Backdrop from '../../UI/Backdrop/Backdrop'
 
-const links = [
-    {to: '/', label: 'Список', exact: true},
-    {to: '/auth', label: 'Авторизация', exact: false}, 
-    {to: '/quiz-creator', label: 'Создать тест', exact: false}, 
-]
+
 
 class Drawer extends Component {
 
@@ -16,7 +12,7 @@ class Drawer extends Component {
         this.props.onClose()
     }
 
-    renderLinks() {
+    renderLinks(links) {
         return links.map((link, index) => {
             return (
                 <li key={index}>
@@ -38,11 +34,23 @@ class Drawer extends Component {
             cls.push(classes.close)
         }
 
+        const links = [
+            {to: '/', label: 'Список', exact: true}
+        ]
+
+        if (this.props.isAuthenticated) {
+            links.push({to: '/quiz-creator', label: 'Создать тест', exact: false})
+            links.push({to: '/logout', label: 'Выйти', exact: false})
+
+        } else {
+            links.push({to: '/auth', label: 'Авторизация', exact: false})
+        }
+
         return (
             <React.Fragment>
             <nav className={cls.join(' ')}>
                 <ul>
-                    { this.renderLinks() }
+                    { this.renderLinks(links) }
                 </ul>
             </nav>
         { this.props.isOpen ? <Backdrop onClick={this.props.onClose} /> : null }
